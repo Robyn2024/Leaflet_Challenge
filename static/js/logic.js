c4357aa33aea5f2a53e3237279874b118e67609e
Robyn2024 <138690945+Robyn2024@users.noreply.github.com>
2024-01-08 15:57:33 -0500

    commit

--- static/js/logic.js
+++ static/js/logic.js
@@ -11,7 +11,7 @@ d3.json(queryUrl).then(function (data) {
 
 // Function to determine marker size
 function markerSize(magnitude) {
-  return magnitude * 3000;
+  return magnitude * 100;
 };
 
 // Function to determine marker color by depth
