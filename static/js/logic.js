d01d4253656d09ac7cf27b02656c751d3234ca6c
Robyn2024 <138690945+Robyn2024@users.noreply.github.com>
2024-01-08 13:11:04 -0500

    update committed

--- static/js/logic.js
+++ static/js/logic.js
@@ -53,9 +53,9 @@ function createFeatures(earthquakeData) {
 function createMap(earthquakes) {
 
   // Define streetmap and darkmap layers
-  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
-    "access_token=pk.eyJ1Ijoia2pnMzEwIiwiYSI6ImNpdGRjbWhxdjAwNG0yb3A5b21jOXluZTUifQ." +
-    "T6YbdDixkOBWH_k9GbS8JQ");
+  var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
 
   // Define a baseMaps object to hold our base layers
   var baseMaps = {
