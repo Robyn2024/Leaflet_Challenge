9d6c65dea29e781ccce81b4dfd0f3e3601b2aac2
Robyn2024 <138690945+Robyn2024@users.noreply.github.com>
2024-01-08 13:52:19 -0500

    adding API key

--- static/js/logic.js
+++ static/js/logic.js
@@ -3,8 +3,10 @@ var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_we
 
 // Perform a GET request to the query URL
 d3.json(queryUrl).then(function (data) {
+
   // Console log the data retrieved 
   console.log(data);
+  
   // Once we get a response, send the data.features object to the createFeatures function.
   createFeatures(data.features);
 });
