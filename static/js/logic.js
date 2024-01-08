3effd1f1ca6d999bddc4fb6fdfe89dd347b27527
Robyn2024 <138690945+Robyn2024@users.noreply.github.com>
2024-01-08 15:29:23 -0500

    commit

--- static/js/logic.js
+++ static/js/logic.js
@@ -68,7 +68,7 @@ function createMap(earthquakes) {
     maxZoom: 18,
     zoomOffset: -1,
     style:    'mapbox/light-v11',
-  
+    access_token: api_key
   });
 
   // Create our map, giving it the grayscale map and earthquakes layers to display on load.
